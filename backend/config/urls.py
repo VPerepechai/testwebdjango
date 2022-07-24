969a8ecff7e8b90b48acdc6e83c8787d974702ee
Vlad Perepechai <perepechai2015@gmail.com>
2022-07-24 16:03:26 +0300

    Added: Change title

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='Two Demo API')
+schema_view = get_swagger_view(title='VPerepechai API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
